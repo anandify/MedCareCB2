d61605c332ff4d275cadc42363b7378d2a611556
Aniket Anand <41674276+anandify@users.noreply.github.com>
2023-06-06 02:14:48 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -203,11 +203,11 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
       if (!isRecognizing) {
         recognition.start();
         isRecognizing = true;
-        micButton.innerHTML = '<img src="${mic}" alt="microphone" />';
+        micButton.innerHTML = `<img src=${mic} alt="microphone" />`;
       } else {
         recognition.stop();
         isRecognizing = false;
-        micButton.innerHTML = '<img src="${mic2}" alt="microphone" />';
+        micButton.innerHTML = `<img src=${mic2} alt="microphone" />`;
       }
     });
     
